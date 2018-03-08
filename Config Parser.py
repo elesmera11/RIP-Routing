00716aceb6b359ef49039920a76bb98d671ab17e
shankoo <37008460+shankoo@users.noreply.github.com>
2018-03-09 11:54:40 +1300

    needs to implement check validity for output

--- Config Parser.py
+++ Config Parser.py
@@ -1,6 +1,7 @@
 import configparser
 
 def get_config(filename):
+    config_list = []
     config =configparser.ConfigParser()
     config.read(filename)
     router_id = int(config.get('Router', 'router-id'))
@@ -8,19 +9,35 @@ def get_config(filename):
     if router_id < 1 or router_id > 64000:
         print("Router ID must be between 1 and 64000")
         return
+    config_list.append(router_id)
+    print(config_list)
     input_ports = config.get('Router', 'input-ports').split(" ")
     
+    input_port_list = []
     for port in input_ports:
         if int(port) < 1024 or int(port) > 64000:
             print("Port number must be between 1024 and 64000")
             return
+        else:
+            input_port_list.append(port)
         
     if len(set(input_ports)) != len(input_ports):
         print("Duplicate port number")
         return
+    config_list.append(input_port_list)
+    print(config_list)
+    print(config_list[1][1])
    
+    output_list = []
     outputs = config.get('Router', 'outputs').split(" ")
-    print(outputs)
+    for output in outputs:
+        output_data = output.split("-")
+        output_list.append(output_data)
+    config_list.append(output_list)
+    print(config_list)
+    print(config_list[0])
+    print(config_list[1])
+    print(config_list[2])
     return
 
 filename = "config_1.ini"
