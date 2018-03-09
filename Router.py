786b0f6c3531251b777b6b2c9478b2c3bea8238a
shankoo <37008460+shankoo@users.noreply.github.com>
2018-03-10 12:41:01 +1300

    Updated socket creations

--- Router.py
+++ Router.py
@@ -1,8 +1,38 @@
 class Router:
-    INFINITY = 16
-    MIN_HOP = 0
+    HOST = "127.0.0.1"
     router_id = 0
-    routing_table = Routing_Table()
+    inputs = []
+    outputs = []
+    routing_table = dict()
+    neighbours = dict()
+    
+    def __init__(self, config_list):
+        self.router_id = config_list[0]
+        for port in config_list[1]:
+            socket = create_socket(port)
+            self.inputs.append(socket)
+        for port, cost, router in config_list[2]:
+            self.neighbours[router] = int(port), int(cost)
+            socket = create_socket(port)
+            self.outputs.append(socket)
+        return
+    
+    # Get neighbour's metric
+    def get_neighbour_metric(self, router_id):
+        return self.neighbours[router_id][1]
+    
+    # Get neighbour's port
+    def get_neighbour_port(self, router_id):
+        return self.neighbours[router_id][0]
+    
+    # Creates sockets
+    def create_socket(self, port):
+        socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socket.setblocking(False)
+        socket.bind((HOST, port))
+        print("Socket " + str(port) + " created")
+        return socket
     
     # Sends packet
     def send_packet(packet):
